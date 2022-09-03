054baca57b0fe991db29b648270689be27a04a42
Jimmy Valencia Urbano <90942748+jvalu90@users.noreply.github.com>
2022-09-03 19:15:40 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+class Main {
+  public static void main(String[] args) {
+    //Solution 1929 ------------------------------------------------------------------------------------
+    Solution1929 solution1929 = new Solution1929();
+    System.out.println(solution1929.getConcatenation(new int[]{1, 2, 1}));    
+    System.out.println(solution1929.getConcatenation(new int[]{1, 3, 2, 1}));      
+  }
+
+}
