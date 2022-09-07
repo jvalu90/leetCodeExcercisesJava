a500735fb822bde091d60e38d9ec7faa098307e6
Jimmy Valencia Urbano <90942748+jvalu90@users.noreply.github.com>
2022-09-07 01:38:38 +0000

    September 06 - Build Array from Permutation. Practice 1920

--- Main.java
+++ Main.java
@@ -1,9 +1,14 @@
 class Main {
   public static void main(String[] args) {
     //Solution 1929 ------------------------------------------------------------------------------------
-    Solution1929 solution1929 = new Solution1929();
-    System.out.println(solution1929.getConcatenation(new int[]{1, 2, 1}));    
-    System.out.println(solution1929.getConcatenation(new int[]{1, 3, 2, 1}));      
+    //Solution1929 solution1929 = new Solution1929();
+    //System.out.println(solution1929.getConcatenation(new int[]{1, 2, 1}));    
+    //System.out.println(solution1929.getConcatenation(new int[]{1, 3, 2, 1}));
+
+    //Solution 1920
+    Solution1920 solution1920 = new Solution1920();
+    System.out.println(solution1920.buildArray(new int[]{0,2,1,5,3,4}));
+    System.out.println(solution1920.buildArray(new int[]{5,0,1,2,3,4}));
   }
 
 }
