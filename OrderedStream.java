6f61e4790e439d4fe8262dd1039605c9dcb0273b
Jimmy Valencia Urbano <90942748+jvalu90@users.noreply.github.com>
2023-01-05 01:27:48 +0000

    January 04. Design an Ordered Stream. Exercise 1656

--- OrderedStream.java
+++ OrderedStream.java
@@ -10,31 +10,32 @@ Implement the OrderedStream class:
     OrderedStream(int n) Constructs the stream to take n values.
     String[] insert(int idKey, String value) Inserts the pair (idKey, value) into the stream, then returns the largest possible chunk of currently inserted values that appear next in the order.
 */
+
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
 
 class OrderedStream {
-  private Map<Integer, String> treeMap;
-  
-  public OrderedStream(int n) {
-        this.treeMap = new TreeMap<Integer, String>();
-  }
-    
-  public List<String> insert(int idKey, String value) {
-    this.treeMap.put(idKey, value);
-    List<String> list = new ArrayList<String>();
+    String[] stream;
+    int ptr = 0;
 
-    // Se puede puede iterar con un for para devolver solo lo que esta a la derecha de la llave que se esta ingresando
-    Iterator it = this.treeMap.keySet().iterator();
-    while(it.hasNext()){
-      Integer key = (Integer) it.next();
-      list.add(this.treeMap.get(key));
+    public OrderedStream(int n) {
+        this.stream = new String[n];
     }
 
-    return list;
-  }
+    public List<String> insert(int idKey, String value) {
+        stream[idKey - 1] = value;
 
+        List<String> result = new ArrayList<>();
+        for (int i = ptr; i < stream.length; i++) {
+            String element = this.stream[i];
+
+            if (element == null) {
+                break;
+            }
+            result.add(element);
+            ptr++;
+        }
+
+        return result;
+    }
 }
